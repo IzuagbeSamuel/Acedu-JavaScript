27e09313440219153cdc6947e2fa8c972d37b3fd
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2021-04-26 13:48:41 +0100

    Created my nav bar

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function add (number1 , number2) {
+  let result = number1 + number2;
+  return result;
+}
+
+
+function sub (number1 , number2) {
+  let result = number1 + number2;
+  return result;
+}
+
+
+function divide (number1 , number2) {
+  let result = number1 + number2;
+  return resut;
+}
+
+$(document).ready(function(){
+$("#head").click(function(){
+  alert("Acedu BootCamp");
+})
+});
+
+
+$(document).ready(function(){
+  $("#home").click(function(){
+    prompt("Are sure you want to continue")
+  })
+});
+
+
+$(document).ready(function(){
+  $("#first").click(function(){
+    $("#second").show();
+    $("#first").hide();
+  })
+})
+
+$(document).ready(function(){
+  $("#hide").click(function(){
+    $("#first").show();
+    $("#hide").hide();
+  })
+})
+ 
+
